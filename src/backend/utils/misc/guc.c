31dd76902751b4c89600218789ebed219c6459be
Daniel Gustafsson <daniel@yesql.se>
2021-02-08 23:52:22 +0100

    nss: Support libnss as TLS library in libpq
    This commit contains the frontend and backend portion of TLS support
    in libpq to allow encrypted connections using NSS. The implementation
    is done as a drop-in replacement for the OpenSSL support and leverages
    the same internal API for abstracting library specific details.
    
    A new GUC, ssl_database, is used to identify the NSS database used for
    the serverside certificates and keys. The existing certificate and key
    GUCs are used for providing certificate and key nicknames.
    
    Client side there is a new connection parameter, cert_database, to
    identify the client cert and key. All existing sslmodes are supported
    in the same way as with OpenSSL.
    
    Authors: Daniel Gustafsson, Andrew Dunstan, Jacob Champion
    Reviewed-by: Michael Paquier

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -4410,7 +4410,11 @@ static struct config_string ConfigureNamesString[] =
 		},
 		&ssl_library,
 #ifdef USE_SSL
+#if defined(USE_OPENSSL)
 		"OpenSSL",
+#elif defined(USE_NSS)
+		"NSS",
+#endif
 #else
 		"",
 #endif
@@ -4479,6 +4483,16 @@ static struct config_string ConfigureNamesString[] =
 	},
 
 	{
+		{"ssl_database", PGC_SIGHUP, CONN_AUTH_SSL,
+			gettext_noop("Location of the NSS certificate database."),
+			NULL
+		},
+		&ssl_database,
+		"",
+		NULL, NULL, NULL
+	},
+
+	{
 		{"synchronous_standby_names", PGC_SIGHUP, REPLICATION_PRIMARY,
 			gettext_noop("Number of synchronous standbys and list of names of potential synchronous ones."),
 			NULL,
@@ -4506,8 +4520,10 @@ static struct config_string ConfigureNamesString[] =
 			GUC_SUPERUSER_ONLY
 		},
 		&SSLCipherSuites,
-#ifdef USE_OPENSSL
+#if defined(USE_OPENSSL)
 		"HIGH:MEDIUM:+3DES:!aNULL",
+#elif defined (USE_NSS)
+		"",
 #else
 		"none",
 #endif
